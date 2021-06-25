4474f157519bfc226e07033fc618eab0432e8ece
shapirodaniel <shapirodanieladam@gmail.com>
2021-06-24 19:34:44 -0500

    update: stop / start logic and btn updates to innerText

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// a class in javascript is like a blueprint for an object
+// in this case, we want to create a stopwatch, so our
+// Stopwatch class is the blueprint for how our stopwatch works
+// we keep track of its time (the currentNumber) as well as
+// whether it is currently counting down or not (the isCountingDown boolean)
+// we also supply functions that let us start and stop time
+// as well as updating the start / stop button text so that
+// it matches what we see :)
 class Stopwatch {
 	constructor() {
 		this.currentNumber = 60;
@@ -30,7 +38,15 @@ class Stopwatch {
 	}
 }
 
+// here, we invoke our class to create an "instance" of a stopwatch
+// this is where we use our blueprints to actually build the thing
+// it would be like using an architect's plans to build a house, as the contractor
+
 const stopwatch = new Stopwatch();
+
+// here we write some special functions that "listen" to user input, like clicking the mouse or trackpad on a particular button
+// and we use the feedback from this to call a function that resets our stopwatch, or stops / starts it
+// cool stuff! :D
 
 document.getElementById('restart').addEventListener('click', () => {
 	document.getElementById('currentNumber').innerText = 60;
